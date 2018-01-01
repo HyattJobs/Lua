51c4626024e18c07beb1bfe15f0fcc934d7fbf02
best <718091951@qq.com>
2018-01-01 22:07:08 +0800

    数组

--- 字符串/字符串.lua
+++ 字符串/字符串.lua
@@ -5,3 +5,32 @@ print("\"字符串1是\"",string1)
 
 string2 = 'runoob.com'
 print("字符串2是",string2)
+
+string3 = [["best"]]
+print("字符串3是",string3)
+print("\a")
+
+print(string.gsub("test",'t','b',1))
+
+print(string.find("bestliuxingtang.cn","tang",1))
+
+print(string.reverse("bestliuxingtang"))
+
+print(string.format("the value is:%d",4))
+
+print(string.char(97,98,99,100))
+
+print(string.byte("abcd",4))
+
+print(string.byte("abcd"))
+
+print(string.len("bestliuxingtang"))
+
+print(string.rep("bestliuxingtang ",3))
+
+print("runoob"..".com")
+
+for word in string.gmatch("hello lua user","%a+") do
+	print(word)
+end
+print(string.format("%d, %q", string.match("I have 2 questions for you.", "(%d+) (%a+)")))
